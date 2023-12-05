8b1dede037b4283de32ecafdc59522703be2da5e
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-12-05 20:02:42 +0900

    Fix the test of ActualSwiReader.

--- grib2/tests/actual_swi.rs
+++ grib2/tests/actual_swi.rs
@@ -9,11 +9,10 @@ fn actual_swi() {
     // GRIB2ファイルを読み込みCSVファイルに座標を出力
     let input = "../resources/actual_swi.bin";
     let mut reader = ActualSwiReader::new(input).unwrap();
-    {
-        let handle = stdout().lock();
-        let mut writer = BufWriter::new(handle);
-        reader.debug_info(&mut writer).unwrap();
-    }
+    let handle = stdout().lock();
+    let mut writer = BufWriter::new(handle);
+    reader.debug_info(&mut writer).unwrap();
+    let number_of_points = reader.section3().number_of_data_points();
 
     // 土壌雨量指数を出力
     let output = "../resources/actual_swi_0.csv";
@@ -25,7 +24,7 @@ fn actual_swi() {
         .unwrap();
     let mut writer = BufWriter::new(file);
     writeln!(writer, "longitude,latitude,value").unwrap();
-    write_values(&mut writer, reader.swi_values().unwrap());
+    write_values(&mut writer, reader.swi_values().unwrap(), number_of_points);
 
     // 第一タンクの値を出力
     let output = "../resources/actual_swi_1.csv";
@@ -37,7 +36,11 @@ fn actual_swi() {
         .unwrap();
     let mut writer = BufWriter::new(file);
     writeln!(writer, "longitude,latitude,value").unwrap();
-    write_values(&mut writer, reader.first_tank_values().unwrap());
+    write_values(
+        &mut writer,
+        reader.first_tank_values().unwrap(),
+        number_of_points,
+    );
 
     // 第二タンクの値を出力
     let output = "../resources/actual_swi_2.csv";
@@ -49,10 +52,19 @@ fn actual_swi() {
         .unwrap();
     let mut writer = BufWriter::new(file);
     writeln!(writer, "longitude,latitude,value").unwrap();
-    write_values(&mut writer, reader.second_tank_values().unwrap());
+    write_values(
+        &mut writer,
+        reader.second_tank_values().unwrap(),
+        number_of_points,
+    );
 }
 
-fn write_values(writer: &mut BufWriter<File>, value_iter: Grib2ValueIter<'_>) {
+fn write_values(
+    writer: &mut BufWriter<File>,
+    value_iter: Grib2ValueIter<'_>,
+    number_of_points: u32,
+) {
+    let mut number_of_reads = 0_u32;
     for value in value_iter {
         let value = value.unwrap();
         if value.value.is_some() {
@@ -65,5 +77,12 @@ fn write_values(writer: &mut BufWriter<File>, value_iter: Grib2ValueIter<'_>) {
             )
             .unwrap();
         }
+        number_of_reads += 1;
     }
+    assert_eq!(
+        number_of_points, number_of_reads,
+        "ファイルには{}個のデータが記録されているとされていますが、{}個のデータを読み込みました。\
+        ファイルが誤っているか、バグがあります。",
+        number_of_points, number_of_reads
+    );
 }
